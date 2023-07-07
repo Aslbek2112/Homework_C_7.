572e83c8108f2cd6fd4237ff42ed97f113e74d3e
Mahmiev Aslbek <mahmiyev.aslbek@gmail.com>
2023-07-07 17:26:58 +0300

    Task 50

---
+++ Task 50/Program.cs
@@ -0,0 +1,67 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет
+*/
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+int[,] array = GetArray(rows, columns, 0, 10);
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+
+bool isExists = IsExists(array, number);
+
+if (isExists)
+{
+    Console.WriteLine("Указанное число существует в массиве");
+}
+
+else
+{
+    Console.WriteLine("Указанное число не существует в массиве");
+}
+
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool IsExists(int[,] array, int number)
+{
+    for (int i = 0; i < array.GetLength(0); i ++)
+    {
+        for (int j = 0; j < array.GetLength(1); j ++)
+        {
+            if (array[i, j] == number)
+                return true;
+        }
+    }
+    return false;
+}
